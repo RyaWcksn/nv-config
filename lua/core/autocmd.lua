88026c1f2618bec657bb7f7fcc7dadd5b5364d75
RyaWcksn <pram.aryawcksn@gmail.com>
2025-11-10 10:03:10 +0700

    update

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -1,5 +1,18 @@
 local augroup = vim.api.nvim_create_augroup('user_cmds', { clear = true })
 
+vim.api.nvim_create_autocmd("BufReadPost", {
+	callback = function(args)
+		local mark = vim.api.nvim_buf_get_mark(args.buf, '"')
+		local line_count = vim.api.nvim_buf_line_count(args.buf)
+		if mark[1] > 0 and mark[1] <= line_count then
+			vim.api.nvim_win_set_cursor(0, mark)
+			vim.schedule(function()
+				vim.cmd("normal! zz")
+			end)
+		end
+	end
+})
+
 vim.api.nvim_create_autocmd('FileType', {
 	pattern = { 'help', 'man', 'qf', 'notesfloat', 'findfile' },
 	group = augroup,
